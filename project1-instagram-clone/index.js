148c9a653f0c727d2981575877c65eb26f4e9615
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-06-25 12:24:52 +0530

    update

--- project1-instagram-clone/index.js
+++ project1-instagram-clone/index.js
@@ -1,21 +1,32 @@
-const express= require("express");
-const app=express();
-const port=3000;
-const path=require("path");
-const data=require("./data.json");
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+const instadata = require("./data.json");
+const { render } = require("ejs");
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
-    res.render("home");
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get("/instagram",(req,res)=>{
-    res.render("instagram");
-})
+app.get("/instagram", (req, res) => {
+  res.render("instagram-landing");
+});
 
-app.listen(port,()=>{
-    console.log(`Listening on port ${port}`);
-})
+app.get("/instagram/:username", (req, res) => {
+  const { username } = req.params;
+  const data = instadata[username];
+  if (data) {
+    res.render("instagram", { data });
+  } else {
+    res.render("error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
