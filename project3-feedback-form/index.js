01374e5aa88d632bb2975d5cf24a5ba8de2558f7
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-06-28 00:06:55 +0530

    update

--- project3-feedback-form/index.js
+++ project3-feedback-form/index.js
@@ -2,7 +2,27 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.set('views engine','views');
-app.set('views',path.join(__dirname,'views'));
-app.use(express.static(path.join(__dirname,'public')));
-app.use(express.urlencoded({extended:true}));
+app.set("views engine", "views");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+let feedback = [];
+
+app.get("/", (req, res) => {
+  res.render(feedback);
+});
+
+app.post("/submit", (req, res) => {
+  const { name, email, message } = req.body;
+  feedback.push({ name, email, message });
+  res.redirect("/all");
+});
+
+app.get("/all", (req, res) => {
+  res.render("allfeedbacks", { feedback });
+});
+
+app.listen("3000", () => {
+  console.log("Listening on port");
+});
