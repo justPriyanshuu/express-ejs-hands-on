e73c76e274e5054091fbba00faa1e73842e5f87c
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-06-30 13:59:45 +0530

    update

--- project4-todo-list/index.js
+++ project4-todo-list/index.js
@@ -8,10 +8,23 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
-let todo = [];
+let todos = [];
 
+app.get("/", (req, res) => {
+  res.render("to-do", { todos });
+});
 
+app.post("/submit", (req, res) => {
+  const { task } = req.body;
+  todos.push(task);
+  res.redirect("/");
+});
 
-app.listen(3000,()=>{
-    console.log('Running on Port');
-})
+let dlt = document.querySelector(".dlt-btn");
+dlt.addEventListener("click", () => {
+    
+});
+
+app.listen(3000, () => {
+  console.log("Running on Port");
+});
