f6060cdf585ae440f73286ad5a7705406bb279ff
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-06-30 13:25:42 +0530

    update

---
+++ project4-todo-list/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+let todo = [];
+
+
+
+app.listen(3000,()=>{
+    console.log('Running on Port');
+})
